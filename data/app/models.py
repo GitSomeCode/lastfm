049fc5c04d82201bd973529947614aba4568055a
sangeeta <sjbitcode@gmail.com>
2015-01-25 04:14:34 +0000

    updated Tag model

--- data/app/models.py
+++ data/app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 
-class Quote(models.Model):
+class Tag(models.Model):
   name = models.CharField(max_length = 30, default = "", blank = False)
-  text = models.CharField(max_length = 200, blank = False)
+  count = models.IntegerField(default = 0, blank = False)
   
   def __unicode__(self):
     return "%s" %(self.name[:24])
+  
+  
