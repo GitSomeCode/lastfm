3db6b0ce19ed0a6efba5c54b368e76d98670343e
sangeeta <sjbitcode@gmail.com>
2015-01-25 07:35:14 +0000

    environment file

--- data/main.py
+++ data/main.py
@@ -1,7 +1,8 @@
 import os, unirest
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-from app.models import Tag 
+from app.models import Tag, TagRelation
 from __future__ import division
 import itertools, json
 
-print "this works"
+api_url = "http://ws.audioscrobbler.com/2.0/"
+api_key = "ff51b4c5accb4afdc6dee971884949b3"
--- data/settings.py
+++ data/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Contains the database access settings.
 """
