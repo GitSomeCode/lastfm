92aa62511026bfed6df41da382691f6937035aaf
sangeeta <sjbitcode@gmail.com>
2015-01-26 02:16:09 +0000

    working on getting tag relations

--- data/app/models.py
+++ data/app/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class Tag(models.Model):
   name = models.CharField(max_length = 30, default = "", blank = False)
   count = models.IntegerField(default = 0, blank = False)
-  relation = models.ManyToManyField("self", symmetrical=False, through='TagRelation')
+  relation = models.ManyToManyField("self", symmetrical = False, through ='TagRelation')
   
   def __unicode__(self):
     return "%s" %(self.name[:24])
   
 class TagRelation(models.Model):
-  tag_to = models.ForeignKey('Tag', blank = False, related_name="tag_to")
-  tag_from = models.ForeignKey('Tag', blank = False, related_name="tag_from")
+  tag_to = models.ForeignKey('Tag', blank = False, related_name = "tag_to")
+  tag_from = models.ForeignKey('Tag', blank = False, related_name = "tag_from")
   metric = models.FloatField(default = 0.0000, blank = False)
-  
+
   class Meta:
     unique_together = ('tag_to', 'tag_from')
   
--- data/main.py
+++ data/main.py
@@ -1,37 +1,122 @@
 from __future__ import division
+import django
+from django.db.models import F
 import os, unirest
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.conf import settings
 from app.models import Tag, TagRelation
 import itertools, json
+django.setup()
+##################################
+def calc(exampleTuple, percent):
+  """
+  Calculates metric between two tags.
+  Divides each tag's count by 100 and takes average.
+  Adds this to the percent.
+  Takes average of this total.
+  """
+  #import pdb; pdb.set_trace();
+  tag1count = exampleTuple[0][1]/100
+  tag2count = exampleTuple[1][1]/100
+  tagSum = tag1count + tag2count
+  tagAvg = tagSum/2
+  metric = (percent + tagAvg)/2
+  resultTuple = (exampleTuple[0][0], exampleTuple[1][0], metric)
+  
+  return resultTuple
+##################################
 
-# To use api key, use settings.API_KEY
+def tagsForRelation(tagTuple):
+  """
+  Returns two sets of tag name, tag count.
+  Needed when making a new TagRelation.
+  """
+  # ((tag1name, tag1count), (tag2name, tag2count))
+  return 
+
+# takes in list of tuples [(), (), ..., ()]
+def calcGroup(group):
+  """
+  Calculates the relationship between every distinct pair of tags in an artist group. 
+  Artist group is a list.
+  Tuples in list are ('tag_name', tag_count)
+  """
+  percent = 1/len(group)
+  
+  for i in itertools.combinations(group, 2):
+    
+    relation = calc(i, percent)
+    
+    tag_name1 = relation[0]
+    tag_name2 = relation[1]
+    metric = relation[2]
+    getRelation(tag_name1, tag_name2)
+    
+    # do tagRelation stuff here 
+    relExists = getRelation(tag_name1, tag_name2)
+    if relExists:
+      relExists.metric += metric
+    else:
+      
+    '''
+    if TagRelation exists, then just update the metric, 
+    else create new TagRelation with new metric
+    '''
+##################################    
+def getRelation(name1, name2):
+    """
+    Checks to see is a relation exists between the two names.
+    If it does, return it, else return None.
+    """
+    def t(n1, n2):
+        return TagRelation.objects.get(tag_to__name = n1, tag_from__name = n2)
+    
+    try:
+      return t(name1, name2)
+    except TagRelation.DoesNotExist:
+      try:
+        return t(name2, name1)
+      except TagRelation.DoesNotExist:
+        pass
+    return None
+#################################
+
 
 api_url = "http://ws.audioscrobbler.com/2.0/"
-
-# this is only for one page! 50 artist per page, 20 pages total
-topArtists = unirest.post(api_url, headers={"Accept":"application/json"}, params={"api_key":settings.API_KEY, "method":"chart.gettopartists", "format":"json"})
-
-'''
-topArtists_toprint = json.dumps(GTA.body, indent=2)
-print topArtists_toprint
-'''
-
-result = topArtists.body
 artists = []
 
-for a in result["artists"]["artist"]:
-  print a["name"]
-  artists.append(a["name"])
+def getTopArtists():
+  '''
+  Calls api method "chart.getTopArtists"
+  Stores artist names in list
+  '''
+  topArtists = unirest.post(api_url, headers={"Accept":"application/json"}, params={"api_key":settings.API_KEY, "method":"chart.gettopartists", "format":"json", "limit":"2"})
+  result = topArtists.body
   
-one = artists[0]
+  # making a list of artist names from chart.getTopArtists
+  for a in result["artists"]["artist"]:
+    print a["name"]
+    artists.append(a["name"])
 
-tag = unirest.post(api_url, headers={"Accept":"application/json"}, params={"api_key":settings.API_KEY, "artist":one, "method":"artist.getTopTags", "format":"json"})
 
-tag_body = tag.body
+def artistRelation(artists):    
+  # for each artist, compile a list of tuples with their tag information, save tags to database, create combinations, calc, create TagRelation, and save TagRelation
+  for a in artists:
+    artistsTags = unirest.post(api_url, headers={"Accept":"application/json"}, params={"api_key":settings.API_KEY, "artist":a, "method":"artist.getTopTags", "format":"json"})
+    tag_body = artistsTags.body
 
-tagName = tag_body["toptags"]["tag"][0]["name"]
-tagCount = tag_body["toptags"]["tag"][0]["count"]
+    # empty list to store tag tuples [(name, count), (name, count),..., (name, count)]
+    tag_list = []
 
-tag1 = Tag(name = tagName, count = tagCount)
-tag1.save()
+    for tag_dict in tag_body["toptags"]["tag"]:
+      tag_tuple = (tag_dict["name"], int(tag_dict["count"]))
+      tag_list.append(tag_tuple)
+
+      # add tag to database
+      new_tag, created = Tag.objects.get_or_create(name=tag_tuple[0], count=tag_tuple[1])
+      if created == False:
+        new_tag.count =  tag_tuple[1]
+
+    calcGroup(tag_list)
+
+
